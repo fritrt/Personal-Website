be49a16cca09719e79f529956c5098f3d252941e
Grace Olos <engr.ace07@gmail.com>
2023-04-24 10:56:00 +0800

    Add files via upload

---
+++ portfolio js.js
@@ -0,0 +1,12 @@
+<script>
+    function changeMemoji() {
+      var memoji = document.getElementById("memoji");
+      memoji.src = "happy.png";
+    }
+    
+    function resetMemoji() {
+      var memoji = document.getElementById("memoji");
+      memoji.src = "memoji.png";
+    }
+  </script>
+
